3e777724de32a816b1d8ac733cf0109e0dd9b8c8
Gaetan <gdesmet@partner.auchan.fr>
2020-07-03 22:50:21 +0200

    evol

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = "0.0.0.0";
 // App
 const app = express();
 app.get("/", (req, res) => {
-  res.send("Hello World 2");
+  res.send("Hello World 4 Gaetan");
 });
 
 app.listen(PORT, HOST);
